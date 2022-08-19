fa1191580176b3a8f65f8d091e2a470070586783
Esteban Herrera <111074073+estebanehc@users.noreply.github.com>
2022-08-19 09:29:53 -0400

    Add files via upload

---
+++ todo/auth.py
@@ -0,0 +1,99 @@
+# from crypt import methods
+import functools
+
+from flask import(
+    Blueprint, flash, g, render_template, request, url_for, session, redirect
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash 
+
+from todo.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+#para agregar rutas al blueprint
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c= get_db()  #invocar a la base de datos para comprobar si existe el username
+        error = None
+        c.execute(
+            'select id from user where username = %s', (username,)
+        )
+        if not username:
+            error = 'Username es requerido'
+        if not password:
+            error = 'Password es requerido'
+        elif c.fetchone() is not None:
+            error = 'Usuario {} se encuentra registrado.'.format(username)
+        
+        if error is None:
+            c.execute(
+                'insert into user (username, password) values (%s, %s)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+        c.execute(
+            'select * from user where username= %s', (username,)
+        )
+        user = c.fetchone()
+
+        if user is None:
+            error = 'Usuario y/o contraseña inválida'
+        elif not check_password_hash(user['password'], password):
+            error = 'Usuario y/o contraseña inválida'
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('todo.index'))
+
+        flash (error)
+
+    return render_template('auth/login.html')
+
+#funcion que colocara al usuario dentro de g
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'select * from user where id= %s', (user_id,)
+        )
+        g.user = c.fetchone()
+
+#función que va a proteger nuestras rutas
+def login_required(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if g.user is None:
+                return redirect(url_for('auth.login'))
+
+            return view(**kwargs)
+
+        return wrapped_view
+
+#funcion para el log out
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
